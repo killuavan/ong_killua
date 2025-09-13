ba726767e46809745f1155cf90b6d4fe3426bcef
killuavan <okilluavanarcher@gmail.com>
2025-09-13 13:03:03 +0800

    Update database configuration to use remote freedb.tech database

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql.freedb.tech',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'freedb_killua',
+    'password'	=> 'CJR%wf?!4%Ca#hE',
+    'database'	=> 'freedb_my_student_data',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
