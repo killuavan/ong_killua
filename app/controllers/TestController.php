71dd193d82bd6658003807224e9c756da71dc0bb
killuavan <okilluavanarcher@gmail.com>
2025-09-13 14:16:40 +0800

    Remove TestController - no longer needed

--- app/controllers/TestController.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simple test controller without security restrictions
-class TestController extends Controller {
-    public function index() {
-        echo "Test Controller - Index Method Working!";
-    }
-}
-?>
