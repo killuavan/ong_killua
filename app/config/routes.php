1d34604759ec0377b76c3f02fda9b70c4dc60f83
killuavan <okilluavanarcher@gmail.com>
2025-09-13 13:35:44 +0800

    Set development mode and change default route to Welcome controller for debugging

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,7 +43,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 
-$router->get('/', 'UserController::index');
+$router->get('/', 'Welcome::index');
 $router->match('/user/create', 'UserController::create', ['GET', 'POST']);
 $router->match('/user/update/{id}', 'UserController::update', ['GET', 'POST']);
 $router->get('/user/delete/{id}', 'UserController::delete');
