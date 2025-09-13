57a1b7c8b317c22959c47bb5b2d222de090104a1
killuavan <okilluavanarcher@gmail.com>
2025-09-13 13:42:48 +0800

    Add TestController and update route for debugging

---
+++ app/controllers/TestController.php
@@ -0,0 +1,8 @@
+<?php
+// Simple test controller without security restrictions
+class TestController extends Controller {
+    public function index() {
+        echo "Test Controller - Index Method Working!";
+    }
+}
+?>
