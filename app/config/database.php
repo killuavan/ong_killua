095075d78ff432967e99d345bb5b56951c4c3896
killuavan <okilluavanarcher@gmail.com>
2025-09-20 17:44:00 +0800

    database

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'ong_killua',
+    'username'	=> 'sql12798455',
+    'password'	=> 'BBZuirn7sC',
+    'database'	=> 'sql12798455',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
