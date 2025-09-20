aac444af9454c50a233d6e21e7a1afc976eaa704
killuavan <okilluavanarcher@gmail.com>
2025-09-20 18:08:09 +0800

    data

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798455',
-    'password'	=> 'BBZuirn7sC',
-    'database'	=> 'sql12798455',
+    'username'	=> 'sql12799362',
+    'password'	=> 'Nqn22qJZkl',
+    'database'	=> 'sql12799362',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
